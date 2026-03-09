3e2988d788117072847eb063886948030a83d23f
sahlmh9-dev <sahlmh9@icloud.com>
2026-03-09 17:09:41 +0000

    Authcontroller
    Creation AuthController

---
+++ vault/app/Http/Controllers/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+ public function showRegisterForm()
+
+   {
+    return view('auth.register');
+
+       }
+
+
+public function register(Request $request)
+{
+    $request->validate([
+        'name' => ['required', 'string' , 'max:255'],
+        'email' => ['required','email', 'max:255', 'unique:users'],
+        'password' => ['required', 'string','min:8','confirmed'], 
+    ]);
+
+    $user = User::create([
+        'name'=> $request->name,
+        'email' => $request->email,
+        'password' => bcrypt($request->password),
+    ]);
+
+    Auth::login($user);
+
+    return redirect()->route('dashboard');
+
+    }
+    }
+
