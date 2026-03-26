11a9d2891f50caea1e048762c6c5d3e2b7fbe7da
sahlmh9-dev <sahlmh9@icloud.com>
2026-03-26 16:59:54 +0100

    Logout Added
    from line 37 to line 43

--- vault/app/Http/Controllers/AuthController.php
+++ vault/app/Http/Controllers/AuthController.php
@@ -34,5 +34,12 @@ public function register(Request $request)
     return redirect()->route('dashboard');
 
     }
+public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect ('/login');
+        }
     }
 
