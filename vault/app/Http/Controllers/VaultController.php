257e7f6b83aa8a7b60a79be0f02796595d145e58
sahlmh9-dev <sahlmh9@icloud.com>
2026-03-26 18:17:52 +0100

    Merge branch 'Feature/Sahl/Vaults' into review

--- vault/app/Http/Controllers/VaultController.php
+++ vault/app/Http/Controllers/VaultController.php
@@ -1,77 +1,72 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use App\Models\Vault;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Http\Request;
 
 class VaultController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
-
+        $vaults = Auth::User->Vaults()->withCount('passwordEntries')->latest()->get();
+        return view('vault.index',compact('vaults'));
     }
 
+    /**
+     * Show the form for creating a new resource.
+     */
     public function create()
     {
-        
+        return view('vault.create');
     }
 
+    /**
+     * Store a newly created resource in storage.
+     */
     public function store(Request $request)
     {
-        
+        $request->validate([
+            'name' => ['required', 'string' , 'max:255'],
+            'description' => ['nullable' , 'string' ],
+            'icon' => ['nullable', 'max:50'],
+            'color' => ['nullable' , 'max:7'],
+        ]);
+        Auth::User()->vaults()->create($validatedData);
+        redirect()->route('vaults.index')->with('success', 'Vault created successfully!');
     }
 
+    /**
+     * Display the specified resource.
+     */
     public function show(string $id)
     {
-        $vault = Vault::findOrFail($id);
-
-        if ($vault->user_id !== Auth::id()) {
-            abort(403);
-    }
-    return view('vaults.show', compact('vault'));
+        //
     }
 
+    /**
+     * Show the form for editing the specified resource.
+     */
     public function edit(string $id)
     {
-        $vault = Vault::findOrFail($id);
-
-        if ($vault->user_id !== Auth::id()) {
-            abort(403);
+        //
     }
 
-    return view('vaults.edit', compact('vault'));
-    }
-
+    /**
+     * Update the specified resource in storage.
+     */
     public function update(Request $request, string $id)
     {
-        $vault = Vault::findOrFail($id);
-
-        if ($vault->user_id !== Auth::id()) {
-            abort(403);
-
-        $validated = $request->validate([
-        'name' => 'required|string|max:255',
-    ]);
-
-        $vault->update($validated);
+        //
     }
 
-    return redirect('/vaults')->with('success', 'Vault updated successfully');
-    }
-
+    /**
+     * Remove the specified resource from storage.
+     */
     public function destroy(string $id)
     {
-        $vault = Vault::findOrFail($id);
-
-        if ($vault->user_id !== Auth::id()){
-            abort(403);
+        //
     }
-
-    $vault->delete();
-
-    return redirect('/vaults')->with('success', 'Vault deleted successfully');
-}
 }
