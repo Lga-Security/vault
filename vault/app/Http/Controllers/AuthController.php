9ec6b7843e7a53a651283f430a143a2a352d07c7
Assem1225 <assemmh9@gmail.com>
2026-03-26 18:20:24 +0100

    vault et auth

--- vault/app/Http/Controllers/AuthController.php
+++ vault/app/Http/Controllers/AuthController.php
@@ -3,43 +3,56 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 
 class AuthController extends Controller
 {
- public function showRegisterForm()
-
-   {
-    return view('auth.register');
-
-       }
-
-
-public function register(Request $request)
-{
-    $request->validate([
-        'name' => ['required', 'string' , 'max:255'],
-        'email' => ['required','email', 'max:255', 'unique:users'],
-        'password' => ['required', 'string','min:8','confirmed'], 
-    ]);
-
-    $user = User::create([
-        'name'=> $request->name,
-        'email' => $request->email,
-        'password' => bcrypt($request->password),
-    ]);
-
-    Auth::login($user);
-
-    return redirect()->route('dashboard');
-
-    }
-public function logout(Request $request)
+    public function showRegisterForm()
     {
-        Auth::logout();
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
-        return redirect ('/login');
-        }
+        return view('auth.register');
     }
 
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        Auth::login($user);
+
+        return redirect()->route('dashboard');
+    }
+
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+
+            $request->session()->regenerate();
+
+            return redirect()->intended('/dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'Invalid credentials',
+        ])->onlyInput('email');
+    }
+}
--- vault/app/Http/Controllers/VaultController.php
+++ vault/app/Http/Controllers/VaultController.php
@@ -1,72 +1,77 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Support\Facades\Auth;
+
 use Illuminate\Http\Request;
+use App\Models\Vault;
+use Illuminate\Support\Facades\Auth;
+
 
 class VaultController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        $vaults = Auth::User->Vaults()->withCount('passwordEntries')->latest()->get();
-        return view('vault.index',compact('vaults'));
+
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        return view('vault.create');
+        
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        $request->validate([
-            'name' => ['required', 'string' , 'max:255'],
-            'description' => ['nullable' , 'string' ],
-            'icon' => ['nullable', 'max:50'],
-            'color' => ['nullable' , 'max:7'],
-        ]);
-        Auth::User()->vaults()->create($validatedData);
-        redirect()->route('vaults.index')->with('success', 'Vault created successfully!');
+        
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //
+        $vault = Vault::findOrFail($id);
+
+        if ($vault->user_id !== Auth::id()) {
+            abort(403);
+    }
+    return view('vaults.show', compact('vault'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
-        //
+        $vault = Vault::findOrFail($id);
+
+        if ($vault->user_id !== Auth::id()) {
+            abort(403);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+    return view('vaults.edit', compact('vault'));
+    }
+
     public function update(Request $request, string $id)
     {
-        //
+        $vault = Vault::findOrFail($id);
+
+        if ($vault->user_id !== Auth::id()) {
+            abort(403);
+
+        $validated = $request->validate([
+        'name' => 'required|string|max:255',
+    ]);
+
+        $vault->update($validated);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+    return redirect('/vaults')->with('success', 'Vault updated successfully');
+    }
+
     public function destroy(string $id)
     {
-        //
+        $vault = Vault::findOrFail($id);
+
+        if ($vault->user_id !== Auth::id()){
+            abort(403);
     }
+
+    $vault->delete();
+
+    return redirect('/vaults')->with('success', 'Vault deleted successfully');
+}
 }
